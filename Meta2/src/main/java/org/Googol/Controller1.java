5d1db03aea73f7cd517aa8261921cdb5154bbd3f
Eduardo Figueiredo <figueiredo92002@gmail.com>
2023-05-04 19:35:53 +0100

    pontape de saida para o git

--- Meta2/src/main/java/org/Googol/Controller1.java
+++ Meta2/src/main/java/org/Googol/Controller1.java
@@ -1,7 +1,12 @@
 package org.Googol;
 
+import org.Googol.forms.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.ui.Model;
 
 
 @Controller
@@ -13,8 +18,19 @@ public class Controller1 {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("user", new User());
+
         return "login";
+    }
+
+    @PostMapping("/save-user")
+    public String saveUserSubmission(@ModelAttribute User user) {
+
+        // TODO: save project in DB here
+        System.out.println(user.getName() + " " + user.getPassword());
+
+        return "result";
     }
 
     
